9fd5a933087f72fd0c1da913987041e8c9fc3ae5
jabbott13153 <jabbott19@my.whitworth.edu>
2018-12-11 13:18:25 -0800

    Updating main.cpp
    Adding additional comments to the program

--- NBody_v5(final)/main.cpp
+++ NBody_v5(final)/main.cpp
@@ -1,9 +1,9 @@
-#include"body.h"
-#include<iostream>
-#include<random>
+#include"body.h" //add user defined class
+#include<iostream> //use for printing coordinates out to console
+#include<random> //used for random engine
 #include<time.h>
 using namespace std;
-#define N 10
+#define N 10 //the number of celestial bodies to be simulated
 
 //////////////////////////////////////////
 // Random int Generation 
@@ -23,7 +23,9 @@ double cs273_rand() {
 }
 ////////////////////////////////////////////
 int main(){
+    //create an array to hold all of the bodies that will be simulated using this program. The amount depends on N
     Body* objects[N];
+    //dynamically allocate memory for body creation. Use the random engine to generate unique coordinates for each body within the coordinates between (0, 1000)
     for(int i = 0; i < N; i++){
         objects[i] = new Body(float(cs273_rand()), float(cs273_rand()));
     }
@@ -38,23 +40,22 @@ int main(){
     // objects[1] = b2;
 
 
-    const float t = 0.01; //the time step in seconds
-    float time_passed = 0.0;
+    const float t = 0.01; //the time step in seconds. Each iteration will increase time passed by this amount. 
+    float time_passed = 0.0; //how much time has passed since the simulation has started. This is used for calculating velocity and displacement of each body.
 
-    
-    //add<<<1,100>>>(dev_a, dev_time);
-
-for( int i = 0; i < 200000; i++){
-        for(int j = 0; j < N; j++){
-            for(int k = 0; k < N; k++){
-                if(j != k){
-            objects[j]->update(objects[k], t); //void update_pos(Body b, float nf, float t)
+    //for each body in the simulation, calculate the net force, velocity, and position.
+for( int i = 0; i < 200000; i++){ //how many timesteps the program will iterate over.
+        for(int j = 0; j < N; j++){ // j and k are used to ensure that the body does not do any calculations on itself
+            for(int k = 0; k < N; k++){/ j and k are used to ensure that the body does not do any calculations on itself
+                if(j != k){ //make sure that the body is not doing calculations on itself
+            objects[j]->update(objects[k], t); //update the body's velocity, position, and net force for the current timestep
                 }
             }
-        time_passed += t;
+        time_passed += t; //once all of the bodies have had their velocity, postion, and netforce calculated, increase the time passed by one timestep/
         }
     }
 
+    //For each body, print out the x and y coordinates to the screen using body.h member function where_am_i()
     for(int i = 0; i < N; i++){
         objects[i] -> where_am_i();
         cout << endl;
